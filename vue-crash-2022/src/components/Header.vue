2a36052e2abdee3f9764499e648fea170f2b2a66
mohammadshhadeh <mohammadzaidsh@outlook.com>
2022-06-04 22:24:17 +0300

    to do app

---
+++ vue-crash-2022/src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  // props: ['title'].
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  }
+  // props: {
+  //   title: {
+  //     type: String,
+  //     default: "Task Tracker",
+  //   },
+  // },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
